4250951085bfcabcb798adfc14d955336177e2f6
Abu Zafar Emon <zafar emon>
2023-06-09 20:41:25 +0600

    event section completed

---
+++ src/components/UpcomingEvent/UpcomingEvent.jsx
@@ -0,0 +1,20 @@
+const UpcomingEvent = () => {
+  return (
+    <section className="bg-white py-10 px-5">
+      <div className="container mx-auto">
+        <div className="w-full lg:w-10/12 mx-auto">
+          <div>
+            <h2 className="text-2xl md:text-3xl text-[#115c8f] font-bold uppercase tracking-[.3em] md:tracking-[.4em]">
+              UPCOMING EVENT
+            </h2>
+          </div>
+          <div className="mt-10 text-center">
+            <p>No event Found</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UpcomingEvent;
