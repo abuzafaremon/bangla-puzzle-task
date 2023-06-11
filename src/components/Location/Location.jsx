e843f32ebef86b721f236057817eeb0be0a40e1a
Abu Zafar Emon <zafar emon>
2023-06-11 20:32:14 +0600

    location section completed

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,18 +1,33 @@
+import map from "../../assets/images/bsa-maps.png";
 const Location = () => {
   return (
-    <section className="py-10 px-5">
+    <section className="py-10 px-5 pb-20">
       <div className="container mx-auto">
         <div className="w-full mx-auto lg:w-10/12 flex flex-col md:flex-row items-center gap-5">
           <div className="w-full">
-            {/* <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.514704915942!2d90.39837257404453!3d23.729018289599374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8efa65e6b1b%3A0x2290178cf2c42315!2sBangladesh%20Shishu%20Academy!5e0!3m2!1sen!2sbd!4v1686414900323!5m2!1sen!2sbd"
-              width="600"
-              height="450"
-              style="border:0;"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe> */}
+            <img className="w-full" src={map} alt="map" />
+          </div>
+          <div className="w-full flex flex-col sm:flex-row md:flex-col gap-5">
+            <div className="text-center w-full">
+              <h2 className="text-2xl md:text-3xl font-medium">
+                Present Address
+              </h2>
+              <p className="text-lg">
+                <span>Present Address- Bangladesh Shishu Academy,</span>
+                <br />
+                <span> Doyel Chattor, Shabag, Dhaka-1000</span>
+              </p>
+            </div>
+            <div className="text-center w-full">
+              <h2 className="text-2xl md:text-3xl font-medium">
+                Permanent Address
+              </h2>
+              <p className="text-lg">
+                <span>Permanent Address- Mymensingh District</span>
+                <br />
+                <span> Mymensingh, Bangladesh</span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
